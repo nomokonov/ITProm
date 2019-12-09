18457cc5cc1a9b45686fb913aff80d7534823ce4
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-12-10 02:19:18 +0900

    cannot find alg for tree rebuild"

--- src/main/java/ru/nomokonov/employeers/service/DepartmentServiceImpl.java
+++ src/main/java/ru/nomokonov/employeers/service/DepartmentServiceImpl.java
@@ -6,11 +6,10 @@ import ru.nomokonov.employeers.model.Department;
 import ru.nomokonov.employeers.repository.DepartmentRepository;
 
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
-public class DepartmentServiceImpl implements  DepartmentService {
+public class DepartmentServiceImpl implements DepartmentService {
 
     private DepartmentRepository departmentRepository;
 
@@ -21,34 +20,51 @@ public class DepartmentServiceImpl implements  DepartmentService {
 
     @Override
     public List<Department> findAll() {
-        return sortInTree(departmentRepository.findAll());
+        return getTree(departmentRepository.findAll());
     }
 
-    private List<Department> sortInTree(List<Department> all) {
-        List<Department> sorted_departments = null;
-        for (Department department: all) {
-//            $query = mysql_query("SELECT * FROM `table`");
-//            $category = array();
-//            while ($row = mysql_fetch_array($query)) {
-//                $category[$row['ParentID']][] = $row;
-//            }
-//
-//            function outputMenu($parent_id, $level) {
-//                global $category;
-//
-//                if (isset($category[$parent_id])) {
-//                    foreach ($category[$parent_id] as $value) {
-//                        echo "<div style=\"margin-left:" . ($level * 25) . "px;\">" . $value['CatName'] . "</div>";
-//                        $level = $level + 1;
-//                        outputMenu($value['id'], $level);
-//                        $level = $level - 1;
-//                    }
-//                }
-//            }
-//            outputMenu(0, 0);
+    private List<Department> getTree(List<Department> all) {
+        List<Department> root_department = new LinkedList<>();
+        Long level = 0L;
+        Iterator<Department> iterator = all.iterator();
+        while (iterator.hasNext()) {
+            Department department = iterator.next();
+            if (department.getParent() == null) {
+                root_department.add(department);
+                iterator.remove();
+            }
         }
-        return departments;
+        iterator = all.iterator();
+        while (iterator.hasNext()) {
+            Department department = iterator.next();
+             int count = 0;
+            for (Department depart: root_department) {
+                count++;
+//            findChilds(all, root_department, department);
+                if ( count == root_department.size() ){
+                    root_department.add(department);
+                    all.remove(department);
+                    break;
+                }
+                if ( department.getId() == depart.getParent().getId() ){
+                    root_department.add(count -1,depart);
+                    all.remove(depart);
+                }
+            }
+        }
+
+        return root_department;
     }
+
+//    private void findChilds(List<Department> all, List<Department> root_department, Department department) {
+//        for (Department depart : all) {
+//            if (department.getId() == depart.getParent().getId()) {
+//                root_department.add(root_department.indexOf(department), depart);
+//                all.remove(depart);
+//                findChilds(all, root_department, depart);
+//            }
+//        }
+//    }
 
     @Override
     public void add(String departmentName, String notice, Long departmentParent) {
@@ -67,7 +83,7 @@ public class DepartmentServiceImpl implements  DepartmentService {
             parent = departmentRepository.getOne(departmentParent);
         }
         Department departmentFromDB = departmentRepository.getOne(departmentId);
-        if ( departmentFromDB != null ){
+        if (departmentFromDB != null) {
             departmentFromDB.setName(departmentName);
             departmentFromDB.setNotice(departmentNotice);
             departmentFromDB.setParent(parent);
@@ -80,10 +96,10 @@ public class DepartmentServiceImpl implements  DepartmentService {
     @Transactional
     public boolean delete(Long departmentId) {
         Department departmentFromDB = departmentRepository.getOne(departmentId);
-        if(departmentFromDB != null) {
+        if (departmentFromDB != null) {
             Set<Department> childrens = departmentFromDB.getChildren();
-            if (!childrens.isEmpty()){
-                for (Department child: childrens) {
+            if (!childrens.isEmpty()) {
+                for (Department child : childrens) {
                     child.setParent(null);
                     departmentRepository.save(child);
                 }
